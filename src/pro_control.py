8d09c13d489005c12bce3e59e9c5231f8ec08ebe
rverleur <rees.verleur@gmail.com>
2023-02-02 08:20:50 -0800

    Created ProControl class

---
+++ src/pro_control.py
@@ -0,0 +1,16 @@
+class ProControl:
+    """!@brief	This class performs proportional control
+        @details	This class performs proportional control
+                    specifically for the motor position on the
+                    motors in the ME_405 kit
+    """
+    def __init__(self, gain, setpoint, ):
+        """!@brief	The constructor for the motor driver class
+        @details	The constructor takes in initial settings for
+                    gain and setpoint. For a step response the setpoint
+                    should start at 0 and will later be changed by the
+                    set_setpoint method
+        @param	gain The initial gain for the 
+        self.gain = gain
+        self.setpoint = setpoint
+        
