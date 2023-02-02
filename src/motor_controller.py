b3dde39cb4f2e6e2a12da50450e2e0e3cbd5dbbb
rverleur <rees.verleur@gmail.com>
2023-02-02 10:34:46 -0800

    com 4 stuff

--- src/motor_controller.py
+++ src/motor_controller.py
@@ -8,34 +8,36 @@ def main():
     @details	This function uses the pro_control class
                 to control the motor
     """
+    u2 = pyb.UART(2, baudrate=115200)
     enc = EncoderDriver(pyb.Pin.board.PB6, pyb.Pin.board.PB7, 4)
     motor = MotorDriver (pyb.Pin.board.PA10,pyb.Pin.board.PB4,pyb.Pin.board.PB5,3)
     con = ProControl()
     read = enc.read()
     pos = 0
-
-    con.set_Kp(float(input("Enter Gain")))
+    
     con.set_setpoint(int(input("Enter Setpoint")))
-    y = 0
-    time = []
-    position = []
-    while y <128:
-        read,pos = enc.update(read,pos)
-        position.append(pos)
-        time.append(utime.ticks_ms())
-        effort = con.run(pos)
-        #print("Effort = ",effort)
-        if effort<-100:
-            effort = -100
-        elif effort>100:
-            effort = 100
+    while True:
+        con.set_Kp(float(input("Enter Gain")))
+        pos = 0
+        utime.
+        time = []
+        position = []
+        for y in range(128):
+            read,pos = enc.update(read,pos)
+            position.append(pos)
+            time.append(utime.ticks_ms())
+            effort = con.run(pos)
+            #print("Effort = ",effort)
+            if effort<-100:
+                effort = -100
+            elif effort>100:
+                effort = 100
+                
+            motor.set_duty_cycle(effort)
+            utime.sleep_ms(10)
             
-        motor.set_duty_cycle(effort)
-        utime.sleep_ms(10)
-        y+=1
-        
-    for x in range(len(time)):
-        print(time[x],',',position[x])
+        for x in range(len(time)):
+            u2.write(time[x],',',position[x])
         
 if __name__=='__main__':
     main()
